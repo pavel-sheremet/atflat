01673ba00171b546b0fb11157615cb96abaf7338
Pavel Sheremet <pavel-sheremet-dev@yandex.ru>
2020-02-03 16:39:50 +0300

    nav layout was added

---
+++ resources/views/layouts/default/app.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', __('main.title'))</title>
+    <meta name="description" content="@yield('description', __('main.description'))">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="icon" type="image/png" href="/img/favicon.png"/>
+</head>
+<body class="main-dark-purple-bgd">
+<div id="app">
+    @include('layouts.default.nav')
+    <main class="py-4">
+        @yield('content')
+    </main>
+</div>
+</body>
+<script src="{{ asset('js/manifest.js') }}" defer></script>
+<script src="{{ asset('js/vendor.js') }}" defer></script>
+<script src="{{ asset('js/app.js') }}" defer></script>
+</html>
---
+++ resources/views/layouts/default/nav.blade.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-expand-lg navbar-dark main-dark-purple-bgd py-1">
+    <a class="navbar-brand" href="/">
+        <img src="/img/logo_without_bgd.svg" width="40">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <ul class="navbar-nav mr-auto">
+
+            <li class="nav-item @if (Route::currentRouteNamed('welcome')) active @endif">
+                <a class="nav-link" href="{{ route('welcome') }}">{{ __('menu.main') }}</a>
+            </li>
+            @guest
+                @if (!Route::currentRouteNamed('login'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('menu.login') }}</a>
+                    </li>
+                @endif
+                @if (Route::has('register') && !Route::currentRouteNamed('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('menu.register') }}</a>
+                    </li>
+                @endif
+            @else
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('logout') }}"
+                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        {{ __('menu.logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </li>
+
+            @endguest
+        </ul>
+    </div>
+</nav>
