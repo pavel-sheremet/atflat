73f58f5b1a1b90fc968e30b684a91aec821b43e9
Pavel Sheremet <pavel-sheremet-dev@yandex.ru>
2020-02-04 16:26:44 +0300

    nav-bar toggle button add outline-none class

--- resources/views/layouts/default/nav.blade.php
+++ resources/views/layouts/default/nav.blade.php
@@ -2,7 +2,7 @@
     <a class="navbar-brand" href="/">
         <img src="{{ asset('storage/img/logo_without_bgd.svg') }}" width="40">
     </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+    <button class="navbar-toggler outline-none" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
