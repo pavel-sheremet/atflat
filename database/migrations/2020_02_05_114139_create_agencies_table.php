7791886b1dc0a8c8c4e23205c672567e91b237f7
Pavel Sheremet <pavel-sheremet-dev@yandex.ru>
2020-02-05 11:50:59 +0300

    add simple agency & agent entities

---
+++ database/migrations/2020_02_05_114139_create_agencies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAgenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agencies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->references('id')->on('users')->unsigned();
+            $table->string('name');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agencies');
+    }
+}
---
+++ database/migrations/2020_02_05_114811_create_agents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAgentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agents', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->references('id')->on('users')->unsigned();
+            $table->bigInteger('agency_id')->references('id')->on('agencies')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agents');
+    }
+}
