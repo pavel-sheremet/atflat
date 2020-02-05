2103b779d257c90e000850e844ffd210b38fa59c
Pavel Sheremet <pavel-sheremet-dev@yandex.ru>
2020-02-05 13:33:25 +0300

    user model add agencies reverse relation

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function agencies ()
+    {
+        return $this->belongsTo(Agency::class, 'id', 'user_id');
+    }
 }
