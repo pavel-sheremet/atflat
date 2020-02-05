5a9732fb86456fed9a87b04ac20e864fc2f08f6f
Pavel Sheremet <pavel-sheremet-dev@yandex.ru>
2020-02-05 18:46:44 +0300

    add agent store request validator

---
+++ app/Http/Requests/StoreAgent.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreAgent extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'user_id' => \Auth::id(),
+        ]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'agency_id' => 'required|integer',
+            'user_id' => 'unique:agents'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'agency_id.required' => __('agent.page.create.form.errors.agency_id.required'),
+            'user_id.unique' => __('agent.page.create.form.errors.user_id.unique'),
+        ];
+    }
+}
